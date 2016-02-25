de5cf0840e222be2c917369281fdb25ac314ce32
qiuyuanshun <qiuyuanshun@yeah.net>
2016-02-25 09:55:18 +0800

    nwe hello world

---
+++ hello.cpp
@@ -0,0 +1,18 @@
+ ///
+ /// @file    hello.cpp
+ /// @Arthur  lemon(qiuyuanshun@yeah.net)
+ /// @date    2016-02-25 09:48:31
+ ///黑云压城城欲摧，甲光向日金鳞开。
+ ///角声满天秋色里，塞上燕脂凝夜紫。
+ ///半卷红旗临易水，霜重鼓寒声不起。
+ ///报君黄金台上意，提携玉龙为君死。
+ ///
+ //
+#include <iostream>//头文件不带.h，是因为头文件是模板写成的
+#include <stdio.h>                    //模板是不能分出头文件和实现文件
+using namespace std;//名称空间
+int main(int argc, const char *argv[])
+{
+    std::cout << "hello,world" << std::endl;    
+    return 0;
+}
